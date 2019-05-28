33d5c8042da67e317ec9562068b23e1df0854b72
Joseph Andy Feidje <Andy_Softmatech@Josephs-MacBook-Pro.local>
2019-05-28 12:00:25 -0400

    working on th third menu

--- lib/noukod_project_1/clione.rb
+++ lib/noukod_project_1/clione.rb
@@ -8,6 +8,7 @@ end
 
 def list_news
     @menu_array = []
+    
     puts "Welcome to Haïti Scrapping news!"
     @news_type = NoukodProject1::News.today_news   
     
@@ -18,21 +19,38 @@ def list_news
     for i in 0..@menu_array.length - 1
         puts "#{i+1})#{@menu_array[i].strip}"
     end 
+    puts "------------------------------------------------------------------------------------------------------------------"
 end
 
 def second_menu(type)
-    puts NoukodProject1::News.scrape_news_by_type(type)
+    @title_array_2 = []
+    @news_by_type_title = NoukodProject1::News.scrape_news_by_type(type)
+
+    @title_array = @news_by_type_title.strip.split("\n")
+    # puts "#{@title_array}"
+
+    @title_array.each.with_index(1) do |_title, i|
+        @title_array_2 << _title
+    end
+
+    # puts "#{@title_array_2.size}"
+    
+    for j in 0..@title_array_2.size - 1
+        puts "#{j+1})#{@title_array_2[j].strip}"
+    end 
+    puts "------------------------------------------------------------------------------------------------------------------"
 end
 
 
 def main_menu
     input = nil
     while input != "quit"
-        puts "Choose a news type/category to display or type quit to exit :"
+    puts "Choose a news type/category to display or type quit to exit :"
     input = gets.strip.downcase
     if input.to_i > 0
         news = @menu_array[input.to_i - 1]
-        puts "#{news}"
+        puts "#{news} : "
+        puts "------------------------------------------------------------------------------------------------------------------"
         second_menu(news.strip)
     elsif input == "list"
         list_news
--- lib/noukod_project_1/deal.rb
+++ lib/noukod_project_1/deal.rb
@@ -4,6 +4,8 @@
 class NoukodProject1::News
   attr_accessor :category, :title, :description, :title_article, :description_article, :url
 
+  @title_article_ = []
+
   def self.today_news
       # Scrape woot and meh and then return deals based on that data
      self.scrape_news
@@ -27,14 +29,13 @@ end
     suff = "metropole/liste_"+type[0..3].downcase+"_fr.php"
     url = "https://metropolehaiti.com/" + suff
     doc = Nokogiri::HTML(open(url))
-
+    urls = []
+    link = ""
+    articles = {}
     news_by_type = self.new
-    news_by_type.title_article = doc.search("span").css(".article-titre").text
-    news_by_type.description_article = doc.search("span").css(".article-text-2")
-    news_by_type.url = doc.css('table').css('tr').css('td.news-link').css('a')
-    puts news_by_type.url
-    # puts news_by_type.description_article
-    # news_type.title
+    news_by_type.title_article = doc.search("span").css(".article-titre").text.strip unless (doc.search("span").css(".article-titre").text.strip).empty?
+    news_by_type.description_article = doc.search("span").css(".article-text-2").text
+    news_by_type.title_article
   end
 
 end
